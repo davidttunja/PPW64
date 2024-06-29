c1dfb6287775ada03add8ad4e44040e12382d304
Joshua Macias 546456 <jtunja@est.ups.edu.ec>
2024-06-28 20:50:34 -0500

    actualizacion

--- javascript/basic/05_taller/js/script.js
+++ javascript/basic/05_taller/js/script.js
@@ -9,4 +9,17 @@ div_cajita.addEventListener('mousedown', function(valor){
         div_cajita.offsetLeft - valor.clientX,
         div_cajita.offsetTop - valor.clientY
     ]
-})
+}, true)
+
+div_cajita.addEventListener('mouseup', function(valor){
+    es_presionado = false
+}, true)
+
+div_cajita.addEventListener('mousemove', function(valor){
+    valor.preventDefault()
+
+    if(es_presionado){
+        div_cajita.style.left = (valor.clientX + offset[0]) + 'px'
+        div_cajita.style.top = (valor.clientY + offset[1]) + 'px'
+    }
+}, true)
